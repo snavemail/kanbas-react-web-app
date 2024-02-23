103df15468a52829659c148a92086ef0cb92a987
snavemail <liam.jk.evans@gmail.com>
2024-02-23 00:29:12 -0500

    Finish a3

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -6,6 +6,7 @@ import Modules from './Modules';
 import CourseNavigation from './Navigation';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Home from './Home';
+import Assignments from './Assignments';
 
 function Courses() {
   const { courseId } = useParams();
@@ -25,7 +26,7 @@ function Courses() {
             <Route path='Home' element={<Home />} />
             <Route path='Modules' element={<Modules />} />
             <Route path='Piazza' element={<h1>Piazza</h1>} />
-            <Route path='Assignments' element={<h1>Assignments</h1>} />
+            <Route path='Assignments' element={<Assignments />} />
             <Route path='Assignments/:assignmentId' element={<h1>Assignment Editor</h1>} />
             <Route path='Grades' element={<h1>Grades</h1>} />
           </Routes>
